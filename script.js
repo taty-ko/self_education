0af6fb1d1bcc49dcc0acb6e0ab67f9dfade19258
taty-ko <hrkolesnik@gmail.com>
2022-10-05 15:55:15 +0200

    Added (Array.isArray(a) == false && Array.isArray(b) == false).

--- script.js
+++ script.js
@@ -6,76 +6,243 @@ class Error {
 }
 
 function plus(a, b) {
-    if ((typeof(a) !== 'string' && typeof(b) !== 'string') ||
-        (typeof(a) !== 'number' && typeof(b) !== 'number') || 
-        (typeof(a) !== Array.isArray([]) && typeof(b) !== Array.isArray([]))) {
+    if ((typeof(a) !== 'string' && typeof(b) !== 'string') &&
+        (typeof(a) !== 'number' && typeof(b) !== 'number') && 
+        (Array.isArray(a) == false && Array.isArray(b) == false)) {
         throw new Error('Arguments are not valid.')
     } 
         let result = a + b;
         return result;
 }
-//тест для кейсов со строкой
-function testStr() {
-    try {
-       (plus('str', 2) ||
-        plus('str', []) ||
-        plus('str', {}) ||
-        plus(2, 'str') ||
-        plus([], 'str') ||
-        plus({}, 'str'))
-    }
-    catch(error) {
-        console.log(error.message);
-    }
-}
-console.log(testStr());
-//тест для кейсов с объектами
-function testObj() {
-    try {
-        (plus({}, 2) ||
-        plus({}, []) ||
-        plus({}, {}) ||
-        plus(2, {}) ||
-        plus([], {}) ||
-        plus({}, {}))
-    }
-    catch(error) {
-        console.log(error.message);
-    }
-}
-//тест для кейсов с массивами
-function testArr() {
-    try {
-        (plus([], 2) ||
-        plus([], {}) ||
-        plus(2, []) ||
-        plus({}, []))
-    }
-    catch(error) {
-        console.log(error.message);
-    }
-}
-//тест для кейсов с пустой строкой
-function testEmpty() {
-    try {
-       (plus('', 'str') || 
-        plus('', {}) || 
-        plus('', []) || 
-        plus('', 10) || 
-        plus('str', '') || 
-        plus({}, '') || 
-        plus([], '') || 
-        plus(10, ''))
-    }
-    catch(error) {
-        console.log(error.message);
-    }
-}
-
-console.log(testStr());
-console.log(testArr());
-console.log(testObj());
-console.log(testEmpty());
 let positive = plus(10, 1);
 console.log(positive);
+console.log(plus('aaaa', 8));
+
+//тест для кейсов со строкой
+function testStrNum() {
+    try {
+       plus('str', 2);
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testStrNum());
+
+function testStrArr() {
+    try {
+       plus('str', [])
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testStrArr());
+
+function testStrObj() {
+    try {
+        plus('str', {})
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testStrObj());
+
+function testNumStr() {
+    try {
+        plus(2, 'str')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testNumStr());
+
+function testArrStr() {
+    try {
+        plus([], 'str')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testArrStr());
+
+function testObjStr() {
+    try {
+        plus({}, 'str')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testObjStr());
+
+//тест для кейсов с объектами
+function testObjNum() {
+    try {
+        plus({}, 2)
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testObjNum());
+
+function testObjArr() {
+    try {
+        plus({}, [])
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testObjArr());
+
+function testObjObj() {
+    try {
+        plus({}, {})
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testObjObj());
+
+function testNumObj() {
+    try {
+        plus(2, {})
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testNumObj());
+
+function testArrObj() {
+    try {
+        plus([], {})
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testArrObj());
+
+//тест для кейсов с массивами
+function testArrNum() {
+    try {
+        plus([], 2)
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testArrNum());
+
+function testNumArr() {
+    try {
+        plus(2, [])
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testNumArr());
+
+//тест для кейсов с пустой строкой
+function testEmptyStr() {
+    try {
+       plus('', 'str')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testEmptyStr());
+
+function testEmptyObj() {
+    try {
+        plus('', {})
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testEmptyObj());
+
+function testEmptyArr() {
+    try {
+        plus('', [])
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testEmptyArr());
+
+function testEmptyNum() {
+    try {
+        plus('', 10)
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testEmptyNum());
+
+function testStrEmpty() {
+    try {
+        plus('str', '')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testStrEmpty());
+
+function testObjEmpty() {
+    try {
+        plus({}, '')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testObjEmpty());
+
+function testArrEmpty() {
+    try {
+        plus([], '')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testArrEmpty());
+
+function testNumEmpty() {
+    try {
+        plus(10, '')
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+console.log(testNumEmpty());
+
+//тест для кейсов с числом и числом
+function testNumNum() {
+    try {
+        let res = plus(10, 10);
+        if (res !== 20) {
+            throw new Error ('invalid.')
+        }
+    }
+    catch(error) {
+        console.log(error.message)
+    }
+}
+console.log(testNumNum());
 
