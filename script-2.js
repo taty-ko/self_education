02bd41e2bec54e8521a0bbbfb2fe51cc4f5a27c2
RTW\tkolisnyk <tkolisnyk@teamworkcommerce.com>
2022-12-18 20:00:43 +0100

    Added new object Store.

--- script-2.js
+++ script-2.js
@@ -1,4 +1,42 @@
 'use strict';
-const arr = [1, 2, 3];
+let arr = [1, 2, 3, 4, 5];
+arr[10] = '234'
+console.log(arr[10]);
+console.log(arr);
 
-console.log(arr[1]);
+
+let obj = {
+    Anna: 500,
+    Alice: 100
+}
+
+let arrToObj = {
+    age: 10,
+    year: 2022,
+    month: 3,
+    days: {
+        holiday: 'Saturday',
+        workingday: 'Monday'
+    }
+}
+
+
+console.log(arrToObj.days.workingday);
+
+const name = 'name';
+arrToObj[name] = 'Anna';
+
+console.log(arrToObj);
+
+
+let store = "Shop";
+let storeDescription = {
+    budget: 10000,
+    employees: ['Mark', 'Sandra', 'Melisa'],
+    products: {
+        shoes: 100,
+        dress: 150
+    },
+    open: true
+}
+console.log(storeDescription);
